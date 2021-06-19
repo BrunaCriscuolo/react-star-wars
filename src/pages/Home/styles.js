7a216e83c5afa43e2b8010d79da637899a3817e2
BrunaCriscuolo <bruna.criscuolo@hotmail.com>
2021-06-19 19:34:13 -0300

    feat: ajustments in background

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -4,26 +4,24 @@ import GlobalStyles from '../../styles/globals';
 
 export const Container = styled.div`
   background-image: url(${GlobalStyles.images.backgroundHome});
-  min-height: 100vh;
 
   .home-padding{
     padding: 0 2rem;
   }
-
   button{
     margin: 2rem 0;
   }
-
   .mt-7{
     margin-top: 2rem;
   }
 
   @media (min-width: 992px){
+    background-size: 100%;
     max-height: 100vh;
+
     .mt-7{
       margin-top: 2.5rem;
     }
-
     .home-padding{
       padding: 0 1rem;
     }
